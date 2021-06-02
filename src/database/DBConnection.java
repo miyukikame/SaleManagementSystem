293cf1d8a8cb09f73847025c4981a95922960540
miyukikame <itamikame@protonmail.com>
2021-06-02 07:24:28 -0500

    added prototype gui for dynamic generation and generalized db connection

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
     public static Connection connectDB()
     {
         try{
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/pe_2021", "root", "sdasdasda");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/pe_2021", "root", "");
         }
         catch(SQLException ex){
             ex.printStackTrace();
