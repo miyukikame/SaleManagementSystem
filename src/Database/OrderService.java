4cef8a0651e435accf102f15e2b452eebb965e0c
arseny1390 <76454978+arseny1390@users.noreply.github.com>
2021-07-02 14:21:18 +0200

    Update OrderService.java
    order service hinzufügt

--- src/Database/OrderService.java
+++ src/Database/OrderService.java
@@ -1,100 +1,52 @@
 package Database;
 
-import Classes.User;
+import Classes.Product;
+import Helper.Cart;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import GUI.ShippingAddress;
 
 import javax.swing.*;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.sql.*;
 
-import Classes.User;
 
-import javax.swing.*;
-import java.sql.*;
-
-public class UserService {
-    static Connection myConn = DBConnection.connectDB();
-
-    public UserService() {
-
-    }
-
-    public static void register(User user) throws SQLException {
-        String sql = "INSERT INTO USER(first_name,last_name,username,email,password)VALUES(?,?,?,?,?)";
-        PreparedStatement statement = myConn.prepareStatement(sql);
-        statement.setString(1, user.getFirstName());
-        statement.setString(2, user.getLastName());
-        statement.setString(3, user.getUsername());
-        statement.setString(4, user.getEmail());
-        statement.setString(5, user.getPassword());
-        statement.executeUpdate();
-    }
-
-    public static User login(String username, String password) throws SQLException {
-        Statement myStatement = myConn.createStatement();
-        ResultSet myResult;
-        if (username.contains("@") && username.contains(".")) {
-            myResult = myStatement.executeQuery("SELECT * FROM USER WHERE email = \"" + username + "\" AND password = \"" + password + "\"");
-        } else {
-            myResult = myStatement.executeQuery("SELECT * FROM USER WHERE username = \"" + username + "\" AND password = \"" + password + "\"");
-        }
-        if (myResult.next()) {
-            JOptionPane.showMessageDialog(null, "Welcome User");
-            return new User(myResult.getInt("user_id"),
-                    myResult.getString("first_name"),
-                    myResult.getString("last_name"),
-                    myResult.getString("username"),
-                    myResult.getString("email"),
-                    password);
-        } else {
-            JOptionPane.showMessageDialog(null, "Invalid Username or Password", "Access denied", JOptionPane.ERROR_MESSAGE);
-
-            return null;
-        }
-    }
-
-    public static void changeInformation(int id,String oldPassword,String newPassword) throws SQLException {
-        Statement myStatement = myConn.createStatement();
-        ResultSet myResult;
-
-        MessageDigest messagedigest = null; //Hash-Funktion Encryption
+public class OrderService {
+    private final static Connection myConn = DBConnection.connectDB();
+    //INESRT ito ORDERS table attributes : user_id,order_date,total_price.
+   static public void InsertOrder() {
+        String sql = "INSERT INTO ORDERS(user_id,order_date,total_price)VALUES(?,?,?)";
         try {
-            messagedigest = MessageDigest.getInstance("SHA-512");
-        } catch (NoSuchAlgorithmException ex) {
-
-        }
-        messagedigest.update(oldPassword.getBytes());
-        StringBuffer sb= new StringBuffer();
-        byte[] b = messagedigest.digest();
-        for(byte b1:b)
-        {
-            sb.append(Integer.toHexString(b1 & 0xff).toString());//yeah
-        }
-        System.out.println(oldPassword);
-        System.out.println(sb.toString());
-        myResult = myStatement.executeQuery("SELECT * FROM USER WHERE password = \"" + sb.toString()+"\"");
-
-        messagedigest = null; //Hash-Funktion Encryption
-        try {
-            messagedigest = MessageDigest.getInstance("SHA-512");
-        } catch (NoSuchAlgorithmException ex) {
-
-        }
-        messagedigest.update(newPassword.getBytes());
-        sb= new StringBuffer();
-        b = messagedigest.digest();
-        for(byte b1:b)
-        {
-            sb.append(Integer.toHexString(b1 & 0xff).toString());//yeah
-        }
-        if(myResult.next())
-        {
-          String sql=  "UPDATE `user` SET `password` = ? WHERE (`user_id` = ?)";
-            PreparedStatement statement = myConn.prepareStatement(sql);
-            statement.setString(1,sb.toString());
-            statement.setInt(2, id);
+            PreparedStatement  statement = myConn.prepareStatement(sql);
+            statement.setInt(1, Cart.user.getId());
+            statement.setString(2, String.valueOf(LocalDate.now()));
+            statement.setString(3,String.valueOf(Cart.totalPrice)+"$");
             statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Fehlernmeldung bei OrderCart Button order :"+e);
         }
+    }
+    //UPDATE product table attributes : quantity
+    static public void UpdateQuantity() {
+        int i = 0;
+        for(Product test:
+                Cart.cartProducts){
+            try {
+                System.out.println(test.getStock());
+                System.out.println(Cart.cartProductQuantity.get(i));
+                String sql = "update product set quantity = ? where product_id = ?";
+                PreparedStatement statement = myConn.prepareStatement(sql);
+                statement.setInt(1, test.getStock() - Cart.cartProductQuantity.get(i));
+                statement.setInt(2, test.getId());
+                statement.executeUpdate();
+                test.setStock(test.getStock() - Cart.cartProductQuantity.get(i));
+                i++;
+            } catch (SQLException e) {
+                System.out.println("Fehler"+e);
+            }
+        }
+    }
 
     }
-}
+
